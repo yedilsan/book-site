4b2a76c0768060579036a8bc99aa07298fa9da13
yedilsan <yedilsanatovv@gmail.com>
2023-11-23 21:38:21 +0600

    damage to orc ex

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 "use strict";
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+class Enemy {
+  health;
+  constructor(health) {
+    this.health = health;
   }
 
-  buy() {
-    console.log("Buy!");
-  }
-}
-
-class AudioBook extends Book {
-  constructor(title, author, lenMin) {
-    super(title, author);
-    this.lenMin = lenMin;
-  }
-
-  log() {
-    console.log(`${this.title} - ${this.lenMin}`);
+  recieveDamage(damage) {
+    this.health = this.health - damage;
+    if (this.health <= 0) {
+      console.log("Enemy dead!");
+      return;
+    }
+    console.log(this.health);
   }
 }
 
-const book = new AudioBook("LotR", "Tolkien", 60 * 20);
-book.log();
-book.buy();
+class Sword {
+  #damage;
+  constructor(damage) {
+    this.#damage = damage;
+  }
+
+  strike(enemy) {
+    enemy.recieveDamage(this.#damage);
+  }
+}
+
+class Orc extends Enemy {
+  constructor(health) {
+    super(health);
+  }
+  recieveDamage(damage) {
+    if (Math.random() > 0.5) {
+      this.health = this.health - damage;
+    }
+    if (this.health <= 0) {
+      console.log("Orc dead!");
+      return;
+    }
+    console.log(this.health);
+  }
+}
+
+const enemy = new Orc(10);
+const sword = new Sword(3);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
+sword.strike(enemy);
