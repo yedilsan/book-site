855a215a7eb501d0d66efcef332ebfab12261b1d
yedilsan <yedilsanatovv@gmail.com>
2023-11-23 21:19:46 +0600

    ES6 class inherit

--- app.js
+++ app.js
@@ -1,42 +1,27 @@
 "use strict";
 
-class User {
-  #login;
-  #_password;
-  constructor(login, password) {
-    this.#login = login;
-    this.#password = password;
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
 
-  set #password(pass) {
-    this.#_password = pass.split("").reverse().join("");
-  }
-  get #password() {
-    return this.#_password.split("").reverse().join("");
-  }
-  get login() {
-    return this.#login;
-  }
-
-  checkPass(pass) {
-    return this.#password === pass;
-  }
-
-  changePass(oldPass, newPass) {
-    if (!this.checkPass(oldPass)) {
-      return false;
-    }
-    this.#password = newPass;
-    return true;
+  buy() {
+    console.log("Buy!");
   }
 }
 
-const user = new User("user@user.use", "123");
-console.log(user);
-console.log(user.login);
+class AudioBook extends Book {
+  constructor(title, author, lenMin) {
+    super(title, author);
+    this.lenMin = lenMin;
+  }
 
-console.log(user.checkPass("321"));
-console.log(user.checkPass("123"));
+  log() {
+    console.log(`${this.title} - ${this.lenMin}`);
+  }
+}
 
-console.log(user.changePass("123", "321"));
-console.log(user);
+const book = new AudioBook("LotR", "Tolkien", 60 * 20);
+book.log();
+book.buy();
