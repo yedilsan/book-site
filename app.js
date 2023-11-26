de7fe181a04074fc696bb772b2d6e06240e80b49
yedilsan <yedilsanatovv@gmail.com>
2023-11-26 14:27:10 +0600

    callback problems

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ request.send();
 request.addEventListener("load", function () {
   const { products } = JSON.parse(this.responseText);
   console.log(products);
-  const sum = products.reduce((acc, p) => (acc += p.price), 0);
-  console.log(sum / products.length);
+
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://dummyjson.com/products/` + products[0].id);
+  request.send();
+  request.addEventListener("load", function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+  });
 });
