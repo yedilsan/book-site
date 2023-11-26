eaa314339a54a1e8f56a7ef9244cc08fab6102d9
yedilsan <yedilsanatovv@gmail.com>
2023-11-26 14:44:39 +0600

    callback fetch

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 "use strict";
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://dummyjson.com/products/");
-request.send();
-
-request.addEventListener("load", function () {
-  const { products } = JSON.parse(this.responseText);
-  console.log(products);
-
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://dummyjson.com/products/` + products[0].id);
-  request.send();
-  request.addEventListener("load", function () {
-    const data = JSON.parse(this.responseText);
+fetch("https://dummyjson.com/products/")
+  .then((response) => response.json())
+  .then(({ products }) => {
+    console.log(products);
+    return fetch(`https://dummyjson.com/products/` + products[0].id);
+  })
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
   });
-});
