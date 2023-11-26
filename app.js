ea6402e3635348a67336452873e0f7ea0e690d85
yedilsan <yedilsanatovv@gmail.com>
2023-11-26 14:20:51 +0600

    sum calculation

--- app.js
+++ app.js
@@ -1,54 +1,12 @@
 "use strict";
 
-class Enemy {
-  health;
-  constructor(health) {
-    this.health = health;
-  }
+const request = new XMLHttpRequest();
+request.open("GET", "https://dummyjson.com/products/");
+request.send();
 
-  recieveDamage(damage) {
-    this.health = this.health - damage;
-    if (this.health <= 0) {
-      console.log("Enemy dead!");
-      return;
-    }
-    console.log(this.health);
-  }
-}
-
-class Sword {
-  #damage;
-  constructor(damage) {
-    this.#damage = damage;
-  }
-
-  strike(enemy) {
-    enemy.recieveDamage(this.#damage);
-  }
-}
-
-class Orc extends Enemy {
-  constructor(health) {
-    super(health);
-  }
-  recieveDamage(damage) {
-    if (Math.random() > 0.5) {
-      this.health = this.health - damage;
-    }
-    if (this.health <= 0) {
-      console.log("Orc dead!");
-      return;
-    }
-    console.log(this.health);
-  }
-}
-
-const enemy = new Orc(10);
-const sword = new Sword(3);
-sword.strike(enemy);
-sword.strike(enemy);
-sword.strike(enemy);
-sword.strike(enemy);
-sword.strike(enemy);
-sword.strike(enemy);
-sword.strike(enemy);
+request.addEventListener("load", function () {
+  const { products } = JSON.parse(this.responseText);
+  console.log(products);
+  const sum = products.reduce((acc, p) => (acc += p.price), 0);
+  console.log(sum / products.length);
+});
