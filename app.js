c8ae46598ae04953c42cea2268e9fae011d707ae
yedilsan <yedilsanatovv@gmail.com>
2023-11-26 15:02:18 +0600

    select category ex

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 "use strict";
 
-fetch("https://dummyjson.com/products/")
-  .then((response) => response.json())
-  .then(({ products }) => {
-    console.log(products);
-    return fetch(`https://dummyjson.com/products/` + products[0].id);
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+/*
+	Сделать запрос на https://dummyjson.com/products/categories,
+	получить список категорий и отобразить <select> выбора категорий.
+*/
+
+function createSelect(array) {
+  const el = document.querySelector(".filter");
+  el.innerHTML = `<select>
+		${array.map((arrEl) => `<option value=${arrEl}>${arrEl}</option>`)}
+	</select>`;
+}
+
+function getCategories() {
+  fetch("https://dummyjson.com/products/categories")
+    .then((response) => response.json())
+    .then((data) => createSelect(data))
+    .catch((error) => console.error(`Ошибка: ${error}`));
+}
+
+getCategories();
