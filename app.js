41ecc3d99a2efa2be2fa6f0617689287123ccfa5
yedilsan <yedilsanatovv@gmail.com>
2023-12-05 11:59:41 +0600

    get my city ex

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 "use strict";
+/*
+	Получить геолокацию пользователя через
+	Geolocation.getCurrentPosition() (WEB API)
+	и по координатам определить город, отправив запрос:
+	https://api.bigdatacloud.net/data/reverse-geocode-client?
+	latitude=00&longitude=00
+*/
 
-async function getProducts() {
-  // fetch("https://dummyjson.com/products")
-  //   .then((response) => response.json())
-  //   .then((data) => console.log(data));
-  const productsResponse = await fetch("https://dummyjson.com/products");
-  const { products } = await productsResponse.json();
-  console.log(products);
-
-  const productResponse = await fetch(
-    `https://dummyjson.com/products/` + products[0].id
-  );
-  const product = await productResponse.json();
-  console.log(product);
+function getMyCoordinates() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        resolve({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+      },
+      (err) => {
+        reject(err);
+      }
+    );
+  });
 }
 
-getProducts();
-console.log("END");
+async function getMyCity() {
+  try {
+    const { latitude, longitude } = await getMyCoordinates();
+    const response = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
+    );
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    console.log(data.city);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+getMyCity();
