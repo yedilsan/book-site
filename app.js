1d6ca28f925d9bc32b9b60edd8d8dab162ed0e38
yedilsan <yedilsanatovv@gmail.com>
2023-12-06 10:56:47 +0600

    activity generator ex

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 "use strict";
 
-async function getAllProducts() {
-  const response = await fetch("https://dummyjson.com/products/");
-  return response.json();
+/*
+	Сделать генератор 3х идей от скуки
+	https://www.boredapi.com/api/activity
+	с отображением на странице
+*/
+const wrapper = document.querySelector(".wrapper");
+
+async function getActivity() {
+  const res = await fetch("https://www.boredapi.com/api/activity");
+  return res.json();
 }
 
-async function getProduct(id) {
-  const response = await fetch("https://dummyjson.com/products/" + id);
-  return response.json();
-}
-
+async function generate() {
+  try {
+    wrapper.innerHTML = "";
+    const data = await Promise.all([
+      getActivity(),
+      getActivity(),
+      getActivity(),
+    ]);
+    console.log(data);
+    for (const el of data) {
+      const element = document.createElement("div");
+      element.innerHTML = `${el.activity}`;
+      wrapper.appendChild(element);
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
