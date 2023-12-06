099e5b31c28cc3e312b8b98a892f260f37aa5a3a
yedilsan <yedilsanatovv@gmail.com>
2023-12-06 10:56:22 +0600

    activity generator

--- app.js
+++ app.js
@@ -10,17 +10,4 @@ async function getProduct(id) {
   return response.json();
 }
 
-async function main() {
-  const { products } = await getAllProducts();
-  const res = await Promise.all(
-    products.map((product) => getProduct(product.id))
-  );
-  console.log(res);
-  // for (const product of products) {
-  // 	const res = await getProduct(product.id);
-  // 	console.log(res);
-  // }
 }
-
-main();
-main();
